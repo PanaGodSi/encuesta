09825fc3fe0cd4b825ae65eaa085cc0691b01fed
PanaGodSi <102960542+PanaGodSi@users.noreply.github.com>
2022-04-05 13:27:16 +0200

    Update encuesta.py

--- encuesta.py
+++ encuesta.py
@@ -1,3 +1,5 @@
 print ("esto es una encuesta")
 nombre=input ("Por favor, indique su nombre:")#hemos creado una variable llamada nombre que va a almacenar el valor preguntado por input
 print ("Hola "+ nombre)
+ciudad=input ("Por favor indique su ciudad de residencia; ")
+print ("Bonita ciudad")
