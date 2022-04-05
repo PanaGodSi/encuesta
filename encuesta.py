554c45a570cff9061e186ebd95f932c79f6f9416
PanaGodSi <102960542+PanaGodSi@users.noreply.github.com>
2022-04-05 13:25:30 +0200

    Add files via upload

---
+++ encuesta.py
@@ -0,0 +1,3 @@
+print ("esto es una encuesta")
+nombre=input ("Por favor, indique su nombre:")#hemos creado una variable llamada nombre que va a almacenar el valor preguntado por input
+print ("Hola "+ nombre)
